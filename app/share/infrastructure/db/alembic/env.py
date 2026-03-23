4f1e43c39d86f6beabad503157caf7e0d80bf47a
Daniel Adrian Galindo Reyes <mseragonf@gmail.com>
2026-03-22 21:58:14 -0600

    refactor: remove leading whitespace from if statement.

--- app/share/infrastructure/db/alembic/env.py
+++ app/share/infrastructure/db/alembic/env.py
@@ -52,7 +52,7 @@ async def run_migrations_online():
     await connectable.dispose()
 
 
- if context.is_offline_mode():
+if context.is_offline_mode():
     run_migrations_offline()
 else:
     asyncio.run(run_migrations_online())
